b24a28b135add93504a9ac415db973db7feb9add
struk49 <andrewstruk@yahoo.co.uk>
2023-06-06 12:31:05 +0000

    added company.json file

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask( __name__)
@@ -10,17 +11,23 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+
+
+
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contacts")
 def contacts():
-    return render_template("contacts.html")
+    return render_template("contacts.html", page_title="Contacts")
 
 
 @app.route("/careera")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 
